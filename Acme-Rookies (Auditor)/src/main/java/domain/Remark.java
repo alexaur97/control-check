b3fb5189287873ae2070db889078d59bdf8481eb
David Morales Llucia <dml94@live.com>
2019-06-21 11:57:02 +0200

    Auditor

--- Acme-Rookies (Auditor)/src/main/java/domain/Remark.java
+++ Acme-Rookies (Auditor)/src/main/java/domain/Remark.java
@@ -6,8 +6,10 @@ import java.util.Date;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -28,6 +30,31 @@ public class Remark extends DomainEntity {
 	private String	picture;
 	private String	mode;
 
+	private Auditor	auditor;
+	private Audit	audit;
+
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	public Audit getAudit() {
+		return this.audit;
+	}
+
+	public void setAudit(final Audit audit) {
+		this.audit = audit;
+	}
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	public Auditor getAuditor() {
+		return this.auditor;
+	}
+
+	public void setAuditor(final Auditor auditor) {
+		this.auditor = auditor;
+	}
 
 	@NotBlank
 	@Pattern(regexp = "yyyy-MM-dd")
